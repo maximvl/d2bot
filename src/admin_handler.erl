1535ef73ea548aab01dbf09d0588958c6fe240db
Maxim V <loz.accs@gmail.com>
2013-05-26 15:19:04 +0000

    added admin page handler

--- src/admin_handler.erl
+++ src/admin_handler.erl
@@ -15,6 +15,14 @@ handle(Req, State) ->
 terminate(_Reason, _Req, _State) ->
   ok.
 
+handle(<<"GET">>, [], Req, State) ->
+  Page = get_html(),
+  {ok, Req2} = cowboy_req:reply(
+                 200,
+                 [{<<"content-type">>, <<"application/json">>}],
+                 Json, Req),
+  {ok, Req2, State};
+
 handle(<<"GET">>, [<<"team">>, Id], Req, State) ->
   Team = matches_db:get_team(Id),
   Props = matches_db:to_proplist(Team),
@@ -46,3 +54,9 @@ handle(<<"POST">>, [<<"team">>, <<"add">>], Req, State) ->
                  [{<<"content-type">>, <<"application/json">>}],
                  Json, Req2),
   {ok, Req3, State}.
+
+get_html() ->
+  {ok, Cwd} = file:get_cwd(),
+  Filename = filename:join([Cwd, "priv", "admin.html"]),
+  {ok, Binary} = file:read_file(Filename),
+  Binary.
